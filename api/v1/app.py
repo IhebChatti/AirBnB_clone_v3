89c76ce1daa464a18909391e07dda8438bf3a06a
KoeusIss <sebri.issam@gmail.com>
2020-09-07 21:39:44 +0100

    commit the stats and the error handling request

---
+++ api/v1/app.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""[python script to set flask app]
+"""
+from flask import Flask, Blueprint, make_response, jsonify
+from models import storage
+from api.v1.views import app_views
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown(error):
+    """teardown method"""
+    storage.close()
+
+@app.errorhandler(404)
+def not_found(error):
+    """Error handling with 404"""
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+if __name__ == "__main__":
+    app.run('0.0.0.0', 5000, threaded=True)
---
+++ api/v1/views/index.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""[a script to route /status on the object app_views]
+"""
+from api.v1.views import app_views
+from models import storage
+from flask import Flask, jsonify
+
+
+@app_views.route('/status', strict_slashes=False)
+def status():
+    """[status route]
+
+    Returns:
+        [json]: [json status]
+    """
+    return jsonify({"status": "ok"})
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def render_stats():
+    """Returns stats about classes"""
+    stats = {
+      "amenities": storage.count('Amenity'), 
+      "cities": storage.count('City'), 
+      "places": storage.count('Place'), 
+      "reviews": storage.count('Review'), 
+      "states": storage.count('State'), 
+      "users": storage.count('User')
+    }
+    return jsonify(stats)
