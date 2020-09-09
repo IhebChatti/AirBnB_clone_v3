bb4358d26a35b2a6c325bffed9e1b915dfe24701
IhebChatti <iheb.chatti@holbertonschool.com>
2020-09-09 13:40:21 +0100

    fixed app.run

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,6 +4,7 @@
 from flask import Flask, Blueprint, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
+from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -14,10 +15,11 @@ def teardown(error):
     """teardown method"""
     storage.close()
 
+
 @app.errorhandler(404)
 def not_found(error):
     """Error handling with 404"""
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == "__main__":
-    app.run('0.0.0.0', 5000, threaded=True)
+    app.run(getenv('HBNB_API_HOST'), getenv('HBNB_API_PORT'), threaded=True)
