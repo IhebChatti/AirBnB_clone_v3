addcdc05991540f37962ff816e4630230fdb6cba
IhebChatti <iheb.chatti@holbertonschool.com>
2020-09-09 14:06:42 +0100

    fix app.run

--- api/v1/app.py
+++ api/v1/app.py
@@ -22,6 +22,6 @@ def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == "__main__":
-    app.run(getenv('HBNB_API_HOST', default='0.0.0.0'),
-            int(getenv('HBNB_API_PORT', default=5000)),
-            threaded=True)
+    api_host = getenv('HBNB_API_HOST', default='0.0.0.0')
+    api_port = getenv('HBNB_API_PORT', default=5000)
+    app.run(host=api_host, port=int(api_port), threaded=True)
